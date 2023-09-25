cb4b14b778db153cf0813ef64787fa9c1a44e3f6
roesnera <adam.roesner@generation.org>
2023-09-25 13:26:56 -0400

    adds routes, functionality for crates

--- src/models.rs
+++ src/models.rs
@@ -20,18 +20,20 @@ pub struct NewRustacean {
     pub email: String,
 }
 
-#[derive(Queryable)]
+#[derive(Queryable, AsChangeset, Deserialize, Serialize)]
 pub struct Crate {
+    #[serde(skip_deserializing)]
     pub id: i32,
     pub rustacean_id: i32,
     pub name: String,
     pub code: String,
     pub version: String,
     pub description: Option<String>,
+    #[serde(skip_deserializing)]
     pub created_at: NaiveDateTime
 }
 
-#[derive(Insertable)]
+#[derive(Insertable, Deserialize, Serialize)]
 #[diesel(table_name=crates)]
 pub struct NewCrate {
     pub rustacean_id: i32,
---
+++ src/rocket_routes/crates.rs
@@ -0,0 +1,48 @@
+use rocket::{serde::json::{Json, Value, serde_json::json}, response::status::{Custom, NoContent}, http::Status};
+
+use crate::{models::{NewCrate,Crate}, repositories::CrateRepository, DbConn};
+
+#[rocket::get("/crates")]
+pub async fn get_crates(db: DbConn) -> Result<Value, Custom<Value>> {
+    db.run(|c| {
+        CrateRepository::find_multiple(c, 100)
+        .map(|crates| json!(crates))
+        .map_err(|_| Custom(Status::InternalServerError, json!("Error")))
+    }).await
+}
+#[rocket::get("/crates/<id>")]
+pub async fn view_crate(id: i32, db: DbConn ) -> Result<Value, Custom<Value>> {
+    db.run(move |c| {
+        CrateRepository::find(c, id)
+        .map(|new_crate| json!(new_crate))
+        .map_err(|_| Custom(Status::InternalServerError, json!("Error")))
+    }).await
+}
+#[rocket::post("/crates", format="json", data="<new_crate>")]
+pub async fn create_crate(new_crate: Json<NewCrate>, db: DbConn) -> Result<Custom<Value>, Custom<Value>> {
+    db.run(move |c| {
+        CrateRepository::create(c, new_crate.into_inner())
+        .map(|a_crate| Custom(Status::Created, json!(a_crate)))
+        .map_err(|_| Custom(Status::InternalServerError, json!("Error")))
+    }).await
+}
+#[rocket::put("/crates/<id>", format="json", data="<a_crate>")]
+pub async fn update_crate(id: i32, a_crate: Json<Crate>, db: DbConn) -> Result<Value, Custom<Value>> {
+    db.run(move |c| {
+        CrateRepository::update(c, id, a_crate.into_inner())
+        .map(|a_crate| json!(a_crate))
+        .map_err(|_| Custom(Status::InternalServerError, json!("Error")))
+    }).await
+}
+#[rocket::delete("/crates/<id>")]
+pub async fn delete_crate(id: i32, db: DbConn) -> Result<NoContent, Custom<Value>> {
+    /* 
+        here we have to move the id into the callback because it may run in a parallel thread or otherwise outlive the containing function block
+        this is not allowed in rust, so we need to specify that the variable will move into the callback and die out here
+     */
+    db.run(move |c| {
+        CrateRepository::delete(c, id)
+        .map(|_| NoContent)
+        .map_err(|_| Custom(Status::InternalServerError, json!("Error")))
+    }).await
+}
