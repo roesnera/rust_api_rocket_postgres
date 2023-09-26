41e99f7b354cb19ad3726ca80acb514267b1ffc6
roesnera <adam.roesner@generation.org>
2023-09-26 15:45:04 -0400

    adds delete test, teardown for rustacean endpoints

--- tests/rustaceans.rs
+++ tests/rustaceans.rs
@@ -4,8 +4,16 @@ use rocket::serde::json::{json, Value};
 #[test]
 fn test_get_rustaceans() {
     let client = Client::new();
+    let rustacean1 = create_test_rustacean(&client);
+    let rustacean2 = create_test_rustacean(&client);
+
     let response = client.get("http://127.0.0.1:8000/rustaceans").send().unwrap();
     assert_eq!(response.status(), StatusCode::OK);
+    let json: Value = response.json().unwrap();
+    assert!(json.as_array().unwrap().contains(&rustacean1));
+    assert!(json.as_array().unwrap().contains(&rustacean2));
+    delete_test_rustacean(&client, rustacean1);
+    delete_test_rustacean(&client, rustacean2);
 }
 #[test]
 fn test_create_rustacean() {
@@ -18,6 +26,8 @@ fn test_create_rustacean() {
         "email": "test@email.com",
         "created_at": rustacean["created_at"]
     }));
+    
+    delete_test_rustacean(&client, rustacean);
 }
 #[test]
 fn test_update_rustacean() {
@@ -38,6 +48,7 @@ fn test_update_rustacean() {
         "email": "test@example.com",
         "created_at": rustacean["created_at"],
     }));
+    delete_test_rustacean(&client, rustacean);
 }
 
 fn create_test_rustacean(client: &Client) -> Value {
@@ -52,6 +63,10 @@ fn create_test_rustacean(client: &Client) -> Value {
     response.json().unwrap()
 }
 
+fn delete_test_rustacean(client: &Client, rustacean: Value) {
+    let response = client.delete(format!("http://127.0.0.1:8000/rustaceans/{}", rustacean["id"])).send().unwrap();
+    assert_eq!(response.status(), StatusCode::NO_CONTENT);
+}
 
 #[test]
 fn test_view_rustacean() {
@@ -60,12 +75,15 @@ fn test_view_rustacean() {
 
     let saved_rustacean: Value = client.get(format!("http://127.0.0.1:8000/rustaceans/{}", rustacean["id"])).send().unwrap().json().unwrap();
     assert_eq!(rustacean, saved_rustacean);
+    delete_test_rustacean(&client, rustacean);
 }
 
 
-// #[test]
-// fn test_delete_rustacean() {
-//     let client = Client::new();
-//     let response = client.get("http://127.0.0.1:8000/rustaceans").send().unwrap();
-//     assert_eq!(response.status(), StatusCode::OK);
-// }
+#[test]
+fn test_delete_rustacean() {
+    let client = Client::new();
+    let rustacean: Value = create_test_rustacean(&client);
+    
+    let response = client.delete(format!("http://127.0.0.1:8000/rustaceans/{}", rustacean["id"])).send().unwrap();
+    assert_eq!(response.status(), StatusCode::NO_CONTENT);
+}
