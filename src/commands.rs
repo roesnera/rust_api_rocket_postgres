6d3b3a5b2fb47f3f3d564ce2a1e27e9d20b14d35
roesnera <adam.roesner@generation.org>
2023-09-28 17:18:31 -0400

    Adds password hashing to cli user creation

--- src/commands.rs
+++ src/commands.rs
@@ -1,3 +1,4 @@
+use argon2::{password_hash::{SaltString, rand_core::OsRng}, PasswordHasher};
 use diesel::{PgConnection, Connection};
 
 use crate::{models::NewUser, repositories::{UserRepository, RoleRepository}};
@@ -9,8 +10,13 @@ fn load_db_connection() -> PgConnection {
 }
 
 pub fn create_user(username: String, password: String, role_codes: Vec<String>) {
-    let new_user = NewUser {username, password};
     let mut c = load_db_connection();
+
+    let salt = SaltString::generate(OsRng);
+    let argon = argon2::Argon2::default();
+    let password_hash = argon.hash_password(password.as_bytes(), &salt).unwrap();
+    
+    let new_user = NewUser {username, password: password_hash.to_string()};
     let user = UserRepository::create(&mut c, new_user, role_codes).unwrap();
     println!("User created {:?}", user);
     let roles = RoleRepository::find_by_user(&mut c, &user).unwrap();
